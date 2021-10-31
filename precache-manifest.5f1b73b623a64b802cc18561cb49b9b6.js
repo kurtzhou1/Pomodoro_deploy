ac6ce93908b42986322ad5e4d82a7ad9780728df
Kurt <pandabat1@gmail.com>
2021-11-01 03:20:30 +0800

    Updates

---
+++ precache-manifest.5f1b73b623a64b802cc18561cb49b9b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "261da1f3d13f4f54915d075e8ac6ec42",
+    "url": "/Pomodoro_deploy/index.html"
+  },
+  {
+    "revision": "af9757fcdac2bcf44cd1",
+    "url": "/Pomodoro_deploy/static/css/main.4dc34f5d.chunk.css"
+  },
+  {
+    "revision": "a5a774febb235c2ae1fc",
+    "url": "/Pomodoro_deploy/static/js/2.6035151f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Pomodoro_deploy/static/js/2.6035151f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af9757fcdac2bcf44cd1",
+    "url": "/Pomodoro_deploy/static/js/main.f4c7cc58.chunk.js"
+  },
+  {
+    "revision": "4c27ed5ae67c1a8d12fe",
+    "url": "/Pomodoro_deploy/static/js/runtime-main.8dd5ec78.js"
+  }
+]);
